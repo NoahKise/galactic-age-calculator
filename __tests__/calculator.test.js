f025cefd333ffc5f74901b9274062d012127b335
noahkise <noah@kisefamily.com>
2023-11-13 10:20:07 -0800

    write SolarAges test1

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -57,8 +57,46 @@ describe('nepCalc', () => {
 })
 
 describe('pluCalc', () => {
-  test('it should take an earth age and convert it to pluto age', () => {
+  test('should take an earth age and convert it to pluto age', () => {
     const noah = new EarthAge(33);
     expect(noah.pluCalc()).toEqual(0.132);
   });
 });
+
+describe('SolarAges', () => {
+  test('should create an object with properties for the solar age on each planet', () => {
+    const noah = new SolarAges(33, 137.511, 53.229, 17.556, 2.772, 1.122, 0.396, 0.198, 0.132);
+    expect(noah.earthAge).toEqual(33);
+    expect(noah.mercAge).toEqual(137.511);
+    expect(noah.venAge).toEqual(53.229);
+    expect(noah.marsAge).toEqual(17.556);
+    expect(noah.jupAge).toEqual(2.772);
+    expect(noah.satAge).toEqual(1.122);
+    expect(noah.urAge).toEqual(0.396);
+    expect(noah.nepAge).toEqual(0.198);
+    expect(noah.pluAge).toEqual(0.132);
+  })
+})
+
+
+
+
+
+// describe('allAgesCalc', () => {
+//   test('should take an earth age and return an object with properties for each planet with key values of their correct ages', () => {
+//     const noah = 33;
+//     expect(allAgesCalc(noah)).toEqual({mercAge: 137.511, venAge: 53.229, marsAge: 17.556, jupAge: 2.772, satAge: 1.122, urAge: 0.396, nepAge: 0.198, pluAge: 0.132});
+//   });
+// });
+
+
+
+
+
+// describe('sinceBDay', () => {
+//   test('should take an earth age as an argument, and a past age as a second argument. then, should find the difference in those two numbers, and calculate how many years that translates to on each planet', () => {
+//     const currentAge = 33;
+//     const pastBDay = 21;
+//     expect(sinceBDay(currentAge, pastBDay)).toEqual({ })
+//   });
+// });
