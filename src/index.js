ab118db1e2fcaaaa37ba6abde42305ccb8655ddc
noahkise <noah@kisefamily.com>
2023-11-13 13:43:38 -0800

    allow since date to print to DOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-import { allAgesCalc, exactAge } from "./calculator.js";
+import { allAgesCalc, exactAge,} from "./calculator.js";
 
-function handleSubmit(e) {
+function handleBirthdaySubmit(e) {
   e.preventDefault();
   const userBDay = new Date(document.getElementById("dateOfBirth").value);
   const todaysDate = new Date();
@@ -32,4 +32,35 @@ function handleSubmit(e) {
 
 }
 
-document.getElementById("birthday").addEventListener("submit", handleSubmit);
+function handleSinceDateSubmit(e) {
+  e.preventDefault();
+  const pastDate = new Date(document.getElementById("pastDate").value);
+  const todaysDate = new Date();
+  const since = exactAge(pastDate, todaysDate);
+  const sinceAges = allAgesCalc(since);
+  const divResult = document.getElementById('result');
+  let earthH3 = document.createElement("h3");
+  let mercH3 = document.createElement("h3");
+  let venH3 = document.createElement("h3");
+  let marsH3 = document.createElement("h3");
+  let jupH3 = document.createElement("h3");
+  let satH3 = document.createElement("h3");
+  let urH3 = document.createElement("h3");
+  let nepH3 = document.createElement("h3");
+  let pluH3 = document.createElement("h3");
+  earthH3.append("It has been " + sinceAges.earthAge + " Earth years since " + pastDate.toLocaleDateString() + ".");
+  mercH3.append("It has been " + sinceAges.mercAge + " Mercury years since " + pastDate.toLocaleDateString() + ".");
+  venH3.append("It has been " + sinceAges.venAge + " Venus years since " + pastDate.toLocaleDateString() + ".");
+  marsH3.append("It has been " + sinceAges.marsAge + " Mars years since " + pastDate.toLocaleDateString() + ".");
+  jupH3.append("It has been " + sinceAges.jupAge + " Jupiter years since " + pastDate.toLocaleDateString() + ".");
+  satH3.append("It has been " + sinceAges.satAge + " Saturn years since " + pastDate.toLocaleDateString() + ".");
+  urH3.append("It has been " + sinceAges.urAge + " Uranus years since " + pastDate.toLocaleDateString() + ".");
+  nepH3.append("It has been " + sinceAges.nepAge + " Neptune years since " + pastDate.toLocaleDateString() + ".");
+  pluH3.append("It has been " + sinceAges.pluAge + " Pluto years since " + pastDate.toLocaleDateString() + ".");
+  divResult.append(earthH3, mercH3, venH3, marsH3, jupH3, satH3, urH3, nepH3, pluH3);
+}
+
+
+document.getElementById("birthday").addEventListener("submit", handleBirthdaySubmit);
+document.getElementById("sinceDate").addEventListener("submit", handleSinceDateSubmit);
+// document.getElementById("birthday").addEventListener("submit", handleSubmit);
