54036ed4ca32f22755f68bcf2b425d809fd65b70
noahkise <noah@kisefamily.com>
2023-11-13 08:37:48 -0800

    write EarthAge test 1

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,6 @@
+describe('EarthAge', () => {
+  test('should create an object with one property for age with an intiger as a value', () => {
+    const noah = new EarthAge(33);
+    expect(noah.age).toEqual(33);
+  });
+});
