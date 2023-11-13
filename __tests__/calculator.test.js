11534f082649279adbea2cf9e9a465d9257dbe0e
noahkise <noah@kisefamily.com>
2023-11-13 08:44:35 -0800

    pass EarthAge test 1

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,3 +1,5 @@
+import { EarthAge } from "../src/calculator.js";
+
 describe('EarthAge', () => {
   test('should create an object with one property for age with an intiger as a value', () => {
     const noah = new EarthAge(33);
---
+++ src/calculator.js
@@ -0,0 +1,5 @@
+export class EarthAge {
+  constructor(age) {
+      this.age = age;
+  }
+}
