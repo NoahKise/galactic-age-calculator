2ec0fa2ca3b68c2003fc266d0ee010d65c67ea61
noahkise <noah@kisefamily.com>
2023-11-13 16:00:44 -0800

    write futNepCal test

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -157,3 +157,10 @@ describe('futUrCal()', () => {
     expect(noah.futUrCal()).toEqual(18534.948);
   });
 });
+
+describe('futNepCal()', () => {
+  test('should find how much of a year is left before the next birthday on Neptune, and multiply that by 60190', () => {
+    const noah = new SolarAges(33.032, 137.664, 53.281, 17.573, 2.775, 1.123, 0.396, 0.198, 0.132)
+    expect(noah.futNepCal()).toEqual(48272.38);
+  });
+});
